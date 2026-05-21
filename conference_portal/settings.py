1f7fcba26760e1724135f5a0885c0f47839f162f
Dilshat-sudo <aptdilshat@gmail.com>
2026-05-21 13:13:23 +0300

    модули 2,3
    стили для всех страничек, добавлен слайдер и лого

--- conference_portal/settings.py
+++ conference_portal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -121,8 +122,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
